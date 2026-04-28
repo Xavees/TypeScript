f1ee864f8c5e4836c908f4efd98d0c76e1919f29
Xavees <luisfernando1337x@gmail>
2026-04-28 16:56:06 -0300

    puxei o readme e adicionei algumas coisas no tS

---
+++ src/tipagem.ts
@@ -0,0 +1,16 @@
+let curso = "TypeScript";
+
+// a tipagem nao permite que usemos a mesma variavel e coloquemos outro valor em cima dela caso nao seja String tambem.
+// curso = 150;
+
+let tecnologias = [
+    "PHP" ,
+    "REACT.JS",
+    "REACT NATIVE",
+    "vue"
+    
+]
+
+tecnologias.push("ANGULAR");
+
+console.log(curso);
---
+++ src/tiposPrimitivos.ts
@@ -0,0 +1,20 @@
+let valor1:Number
+
+valor1 = 30;
+
+console.log(valor1);
+
+let frase:String
+
+frase = "Olá , seja bem vindo. Esse é o tipo String do TypeScript.";
+console.log(frase);
+
+let typeInference 
+
+typeInference = "O typeScript vai deduzir que é tipo String, por eu estar adicionando uma String a variavel";
+
+console.log(typeInference);
+
+let valor3:Number = 40;
+
+console.log("Adicionando o valor na declaração da variavel" + valor3);
